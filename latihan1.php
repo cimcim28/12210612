f7b226655544977ccd76e66d4923016eff0b0cfb
cimcim28 <pmuharrom@gmail.com>
2022-10-16 08:10:12 +0700

    update

---
+++ latihan1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar mahasiswa</title>
+</head>
+<body>
+  <h3>Daftar mahasiswa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NIM</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="kuliah\pertemuan1\img\pemandangan.jpg" width="100"></td>
+      <td>12210612</td>
+      <td>Prisma M</td>
+      <td>pmuharrom@gmail.com</td>
+      <td>sistem informasi</td>
+      <td>
+        <a href="">Ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
